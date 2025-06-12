5820d188a034ccac60e55eb745a5a85f8f78d9e0
PbDNS <jm@delliste.fr>
2025-06-12 18:15:08 +0200

    Update blocklist.py

--- blocklist.py
+++ blocklist.py
@@ -63,7 +63,8 @@ blocklist_urls = [
     "https://adguardteam.github.io/HostlistsRegistry/assets/filter_4.txt",
     "https://raw.githubusercontent.com/easylist/listefr/refs/heads/master/hosts.txt",
     "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt",
-    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_31.txt"
+    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_31.txt",
+    "https://raw.githubusercontent.com/PbDNS/Blocklists/refs/heads/main/add.txt"
 ]
 
 def is_valid_domain(domain):
