d1c6544d75fe16951becadc2d457cd9481e5a749
PbDNS <jm@delliste.fr>
2025-06-12 21:38:01 +0200

    blocklist.py

--- blocklist.py
+++ blocklist.py
@@ -46,7 +46,7 @@ except locale.Error as e:
 
 # Blocklistes incluses
 blocklist_urls = [
-    #"https://raw.githubusercontent.com/PbDNS/Blocklists/refs/heads/main/add.txt",
+    "https://raw.githubusercontent.com/PbDNS/Blocklists/refs/heads/main/add.txt",
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/refs/heads/main/adblock/multi.txt",
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/refs/heads/main/adblock/popupads.txt",
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/refs/heads/main/adblock/native.amazon.txt",
@@ -76,7 +76,6 @@ blocklist_urls = [
     "https://raw.githubusercontent.com/easylist/listefr/refs/heads/master/hosts.txt",
     "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt",
     "https://adguardteam.github.io/HostlistsRegistry/assets/filter_31.txt"
-    #"https://raw.githubusercontent.com/PbDNS/Blocklists/refs/heads/main/add.txt"
 ]
 
 def is_valid_domain(domain):
